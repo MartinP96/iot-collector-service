54468b35e51a44a94f33f2bed77798459c2edde8
Martin Porenta <porenta.matrtin@gmail.com>
2023-02-19 11:12:47 +0100

    MySQL client
    - Added methods to connect and disconnect from MySql server

--- iot_collector_service/sql_client.py
+++ iot_collector_service/sql_client.py
@@ -1,7 +1,33 @@
 from abc import ABC, abstractmethod
+import mysql.connector
+from mysql.connector import Error
 
 class SqlClient(ABC):
-    pass
 
-class MySqlClient(ABC):
-    pass
+    @abstractmethod
+    def connect_sql(self, host: str, database: str, user: str, password: str):
+        pass
+
+    @abstractmethod
+    def disconnect_sql(self):
+        pass
+
+class MySqlClient(SqlClient):
+
+    def __init__(self):
+        self.connection = None
+
+    def connect_sql(self, host: str, database: str, user: str, password: str):
+        try:
+            self.connection = mysql.connector.connect(host=host,
+                                                      database=database,
+                                                      user=user,
+                                                      password=password)
+
+        except mysql.connector.Error as error:
+            print("Failed to connect to server: {}".format(error))
+
+    def disconnect_sql(self):
+        if self.connection.is_connected():
+            self.connection.close()
+            print("MySQL connection is closed")
