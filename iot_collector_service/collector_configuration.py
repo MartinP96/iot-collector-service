dc393a56c6181574b6f98f0b181ea235d20b4d70
MartinP96 <porenta.martin@gmail.com>
2024-08-11 00:04:25 +0300

    Added device name to the collector configuration.

--- iot_collector_service/collector_configuration.py
+++ iot_collector_service/collector_configuration.py
@@ -4,9 +4,11 @@ class CollectorConfiguration:
                  usr: str,
                  password: str,
                  ip_addr: str,
-                 port: int):
+                 port: int,
+                 device_name: str):
         self.configuration_id = configuration_id
         self.usr = usr
         self.password = password
         self.ip_addr = ip_addr
         self.port = port
+        self.device_name = device_name
