66411f6b1d264216d1f163f589f06259be8478b0
MartinP96 <porenta.martin@gmail.com>
2024-08-11 10:52:02 +0300

    Logging collector servite to log file.

--- iot_collector_service/data_collector_service.py
+++ iot_collector_service/data_collector_service.py
@@ -1,6 +1,11 @@
 from .data_collector import IDataCollector
 from abc import ABC, abstractmethod
 import os
+import logging
+from datetime import datetime
+
+logger = logging.getLogger(__name__)
+DATA_LOG_PATH = "collector_logs/"
 
 class IDataCollectorService(ABC):
 
@@ -25,9 +30,19 @@ class DataCollectorService(IDataCollectorService):
         self.collectors_list = []
         self.create_folder_structure()
 
+        # Create datalog
+        logging.basicConfig(filename=f"{DATA_LOG_PATH}log_{datetime.today().strftime('%Y%m%d_%H%M%S')}.log",
+                            format='%(asctime)s %(levelname)-8s %(message)s',
+                            level=logging.INFO,
+                            datefmt='%Y-%m-%d %H:%M:%S')
+
     def add_collector(self, new_collector: IDataCollector, collector_name=""):
         self.collectors_list.append(new_collector)
         self.create_folder_structure()
+
+        # Write to log
+        logging.info(f"Adding collector to collector service: Device Id:{new_collector.device_settings.device_id}, "
+                     f"Device name: {new_collector.device_settings.device_name}")
 
     def start_collection(self):
         status = self.collectors_list[0].connect_collector()
@@ -35,21 +50,44 @@ class DataCollectorService(IDataCollectorService):
             print("Collector connected")
             self.collectors_list[0].run_collector()
             print("Collector service start")
+
+            # Write to log
+            logging.info(f"Starting collection: Device Id:{self.collectors_list[0].device_settings.device_id}, "
+                         f"Device name: {self.collectors_list[0].device_settings.device_name}")
+
         else:
             print("Collector not connected!")
 
+            # Write to log
+            logging.info(f"Collector not connected!: Device Id:{self.collectors_list[0].device_settings.device_id}, "
+                         f"Device name: {self.collectors_list[0].device_settings.device_name}")
+
     def resume_collection(self):
         self.collectors_list[0].subscribe_topic()
+
+        # Write to log
+        logging.info(f"Resuming collection: Device Id:{self.collectors_list[0].device_settings.device_id}, "
+                     f"Device name: {self.collectors_list[0].device_settings.device_name}")
 
     def hold_collection(self):
         self.collectors_list[0].unsubscribe_all_topic()
         print("Collector service held")
 
+        # Write to log
+        logging.info(f"Holding collection: Device Id:{self.collectors_list[0].device_settings.device_id}, "
+                     f"Device name: {self.collectors_list[0].device_settings.device_name}")
+
     def stop_collection(self):
         self.collectors_list[0].stop_collector()
         self.collectors_list[0].disconnect_collector()
-        del self.collectors_list[0]
+
         print("Collector service stop")
+
+        # Write to log
+        logging.info(f"Stopping collection: Device Id:{self.collectors_list[0].device_settings.device_id}, "
+                     f"Device name: {self.collectors_list[0].device_settings.device_name}")
+
+        del self.collectors_list[0]
 
     def get_data(self):
         return self.collectors_list[0].get_data()  # TODO: Naredi da deluje za več kolektorjev
@@ -59,14 +97,14 @@ class DataCollectorService(IDataCollectorService):
 
     def create_folder_structure(self):
         # Create collector log folder
-        if not os.path.exists("collector_logs/"):
-            os.makedirs("collector_logs/")
+        if not os.path.exists(DATA_LOG_PATH):
+            os.makedirs(DATA_LOG_PATH)
 
         # Create measurement data folder
         if not os.path.exists("collector_data/"):
             os.makedirs("collector_data/")
         else:
-            #Create sub folder of each collector
+            # Create sub folder of each collector
             for collector in self.collectors_list:
                 if not os.path.exists(f"collector_data/{collector.device_settings.device_name}"):
                     os.makedirs(f"collector_data/{collector.device_settings.device_name}")
